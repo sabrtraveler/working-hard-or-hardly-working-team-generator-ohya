66c9e8f5ca1f50b52a2a010fd5670df58824a1bd
Robert World <64225836+sabrtraveler@users.noreply.github.com>
2022-02-05 18:58:40 -0500

    added some code in app.js

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-//activate modules
+//add modules
 
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
+
+//add classes modules
+
+
+
+// Using Path module to create an output file into dist folder
+//path resolve -> current working directory / dist = ../working-hard-or-hardly-working-team-generator-ohya/dist
+const outputDir = path.resolve(__dirname, "dist");
+//path join ->  joining outputDir + employees.html = ../outputDir/employees.html
+const outputPath = path.join(outputDir, "employees.html");
